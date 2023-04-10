b7006b0252eaf1ee62254ff5a734e86a64a28fc4
ryiyin <90423334+ryiyin@users.noreply.github.com>
2023-04-09 20:47:23 -0400

    calculator model with basics arithmetics functions

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
-const buttons = document.querySelectorAll(".num")
-const display = document.querySelector(".display")
+const buttons = document.querySelectorAll(".num");
+const display = document.querySelector(".display");
+const operators = document.querySelectorAll(".operator");
+const evalu = document.querySelector(".eval");
+const clear = document.querySelector(".clear");
+let acum = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.innerHTML += button.innerHTML;
+  });
+});
 
 
-buttons.forEach(button => {
-    button.addEventListener('click', ()=>{
-        display.innerHTML += button.innerHTML
-    })
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    if (operator.value === "+" && display.innerHTML !== '') {
+      acum = `${display.innerHTML}+`;
+      display.innerHTML = '';
+    }
+    if (operator.value === "-" && display.innerHTML !== '') {
+      acum = `${display.innerHTML}-`;
+      display.innerHTML = "";
+    }
+    if (operator.value === "*" && display.innerHTML !== '') {
+      acum = `${display.innerHTML}*`;
+      display.innerHTML = "";
+    }
+    if (operator.value === "/" && display.innerHTML !== '') {
+      acum = `${display.innerHTML}/`;
+      display.innerHTML = "";
+    }
+  });
+});
+
+evalu.addEventListener("click", () => {
+  acum.includes("+")
+    ? (display.innerHTML = parseInt(acum) + parseInt(display.innerHTML))
+    : "";
+  acum.includes("-")
+    ? (display.innerHTML = parseInt(acum) - parseInt(display.innerHTML))
+    : "";
+  acum.includes("*")
+    ? (display.innerHTML = parseInt(acum) * parseInt(display.innerHTML))
+    : "";
+  acum.includes("/")
+    ? (display.innerHTML = parseInt(acum) / parseInt(display.innerHTML))
+    : "";
+});
+
+clear.addEventListener("click", () => {
+  display.innerHTML = "";
 });
